7abbd74f9ef668e3db7dbf0db622677894b51307
Ibrahim Al-Salloum <61331054+ibrahim739@users.noreply.github.com>
2021-08-04 07:36:50 +0300

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,20 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - http://py4e-data.dr-chuck.net/known_by_Fikret.html')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+counter = 0
+while counter < len(tags) :
+    link = tags[counter].get('href',None)
+    print('\n',link)
+    counter = counter + 1
